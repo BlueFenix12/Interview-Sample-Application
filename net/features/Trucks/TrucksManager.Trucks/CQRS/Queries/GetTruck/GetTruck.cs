c52114901624471c7c2927b844829a9ffb8df065
BlueFenix12 <bluefenix12@outlook.com>
2024-01-31 22:49:43 +0100

    GetTruck operation was added; Repositories were updated

---
+++ net/features/Trucks/TrucksManager.Trucks/CQRS/Queries/GetTruck/GetTruck.cs
@@ -0,0 +1,12 @@
+﻿using TrucksManager.Common.CQRS;
+using TrucksManager.Trucks.Domain;
+
+namespace TrucksManager.Trucks.CQRS.Queries.SingleTruck;
+
+public static class GetTruck
+{
+    public sealed class Query : IQuery<Truck>
+    {
+        public Guid Id { get; set; }
+    }
+}
---
+++ net/features/Trucks/TrucksManager.Trucks/CQRS/Queries/GetTruck/GetTruckQueryHandler.cs
@@ -0,0 +1,22 @@
+﻿using Ardalis.Result;
+using MediatR;
+using TrucksManager.Trucks.Domain;
+using TrucksManager.Trucks.Infrastructure;
+
+namespace TrucksManager.Trucks.CQRS.Queries.SingleTruck;
+
+public class GetTruckQueryHandler : IRequestHandler<GetTruck.Query, Result<Truck>>
+{
+    private readonly ITrucksRepository repository;
+
+    public GetTruckQueryHandler(ITrucksRepository repository)
+    {
+        this.repository = repository;
+    }
+
+    public async Task<Result<Truck>> Handle(GetTruck.Query request, CancellationToken cancellationToken)
+    {
+        var result = await this.repository.GetTruckAsync(request.Id, cancellationToken);
+        return result;
+    }
+}
