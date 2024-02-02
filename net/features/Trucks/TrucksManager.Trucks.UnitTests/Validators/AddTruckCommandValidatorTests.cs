80a355f68c0d4bacf680bcfe4445561cec1c2f80
BlueFenix12 <bluefenix12@outlook.com>
2024-02-02 10:17:10 +0100

    Unit Tests were added for truck module

---
+++ net/features/Trucks/TrucksManager.Trucks.UnitTests/Validators/AddTruckCommandValidatorTests.cs
@@ -0,0 +1,84 @@
+﻿using Ardalis.Result;
+using TrucksManager.Trucks.CQRS.Commands;
+using TrucksManager.Trucks.Infrastructure;
+
+namespace TrucksManager.Trucks.UnitTests.Validators;
+
+public class AddTruckCommandValidatorTests
+{
+    private readonly Mock<ITrucksRepository> mock;
+    private readonly AddTruckCommandValidator validator;
+
+    public AddTruckCommandValidatorTests()
+    {
+        this.mock = new Mock<ITrucksRepository>();
+        this.validator = new AddTruckCommandValidator(this.mock.Object);
+    }
+
+    [Fact]
+    public async Task Validate_CodeIsInvalid_ValidationFails()
+    {
+        this.mock
+            .Setup(x => x.IsTruckCodeValid(It.IsAny<string>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync(() => Result.Conflict());
+
+        var command = new AddTruck.Command();
+
+        var validatioResult = await this.validator.ValidateAsync(command);
+
+        validatioResult.Should().NotBeNull();
+        validatioResult.IsValid.Should().BeFalse();
+        validatioResult.Errors.Should().NotBeEmpty();
+        validatioResult.Errors.Should().HaveCount(3);
+    }
+    
+    [Fact]
+    public async Task Validate_CodeIsValid_HasBeenTakenByOtherTruck_ValidationFails()
+    {
+        this.mock
+            .Setup(x => x.IsTruckCodeValid(It.IsAny<string>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync(() => Result.Conflict());
+
+        var command = new AddTruck.Command { Code = "bla bla"};
+
+        var validatioResult = await this.validator.ValidateAsync(command);
+
+        validatioResult.Should().NotBeNull();
+        validatioResult.IsValid.Should().BeFalse();
+        validatioResult.Errors.Should().NotBeEmpty();
+        validatioResult.Errors.Should().HaveCount(2);
+    }
+    
+    [Fact]
+    public async Task Validate_CodeIsValidAndNotTaken_NameIsNotValid_ValidationFails()
+    {
+        this.mock
+            .Setup(x => x.IsTruckCodeValid(It.IsAny<string>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync(() => Result.Success());
+
+        var command = new AddTruck.Command { Code = "bla bla" };
+
+        var validatioResult = await this.validator.ValidateAsync(command);
+
+        validatioResult.Should().NotBeNull();
+        validatioResult.IsValid.Should().BeFalse();
+        validatioResult.Errors.Should().NotBeEmpty();
+        validatioResult.Errors.Should().HaveCount(1);
+    }
+    
+    [Fact]
+    public async Task Validate_CodeIsValidAndNotTaken_NameValid_ValidationSuccess()
+    {
+        this.mock
+            .Setup(x => x.IsTruckCodeValid(It.IsAny<string>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync(() => Result.Success());
+
+        var command = new AddTruck.Command { Code = "bla bla", Name = "bla bla"};
+
+        var validatioResult = await this.validator.ValidateAsync(command);
+
+        validatioResult.Should().NotBeNull();
+        validatioResult.IsValid.Should().BeTrue();
+        validatioResult.Errors.Should().BeEmpty();
+    }
+}
